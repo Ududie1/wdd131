949632eee06893aecde461264edd9d373e951c14
Ududie1 <udiesteve@yahoo.com>
2024-09-28 19:55:42 +0100

    com

---
+++ week02/styles/script.js
@@ -0,0 +1,20 @@
+
+        
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('______'); // you need to fill in the blank to reference the HTML element that is unordered list element.
+
+const body = document.body
+
+const div = document.createElement('div')
+//div.innerText = "hello world"
+body.append(div)
+
+//div.append= ("Hello world")
+div.textContent = "Hello world 34"
+
+body.append("hello", "bye", "user")
+
+const div = document.querySelector("div")
+console.log(div.textContent)
+console.log(div.innerText)
